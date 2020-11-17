759a2cb0b1ece2880364a8dfc8bfdc05e88f6982
EC2 Default User <wada.rio1001@gmail.com>
2020-11-17 08:24:51 +0000

    add edit

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -9,8 +9,24 @@ class Admins::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admins_customer_path(@customer), notice: "You have updated customer successfully."
+    else
+      render "edit"
+    end
   end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_nuber, :email, :is_valid)
+  end
+  
+  
+  
+  
 end

85de9146b63fcf4b3e917bf75223435b42de19af
EC2 Default User <wada.rio1001@gmail.com>
2020-11-18 10:15:37 +0000

    Fix path,Add page index

--- config/routes.rb
+++ config/routes.rb
@@ -41,10 +41,10 @@ Rails.application.routes.draw do
     get 'orders' => 'orders#index'
     get 'orders/:id' => 'orders#show'
     get 'cart_items' => 'cart_items#index'
+    post 'cart_items' => 'cart_items#create'
     patch 'cart_items/:id' => 'cart_items#update'
     delete 'cart_items/:id' => 'cart_items#destroy'
     delete 'cart_items/all' => 'cart_items#destroy_all'
-    post 'cart_items' => 'cart_items#create'
     #get 'homes/top'
     get '/about' => 'homes#about'
   end

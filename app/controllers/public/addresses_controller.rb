c7b1147a508f228182fad227c8ed8a9e217e30df
Jinzenji <acfm.air967@gmail.com>
2020-11-21 17:36:51 +0000

    [fix]addresses

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,12 +1,17 @@
 class Public::AddressesController < ApplicationController
+
+  before_action :authenticate_customer!
+
   def index
+    @addresses = current_customer.addresses
+  	@address = Address.new
   end
 
   def create
     @adresses = Address.new(params_adresses)
     @adresses.customer_id = current_customer.id
     @adresses.save
-    redirect_to orders_new_path()
+    redirect_to request.referer
   end
 
   # 住所登録非同期
@@ -24,18 +29,31 @@ class Public::AddressesController < ApplicationController
 
   # 住所選択非同期
   def create3
-      # /addresses/:address_id/create3(.:format)  
+      # /addresses/:address_id/create3(.:format)
       @order = Order.new()
       @address = Address.find(params[:address_id])
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
+	  if @address.update(address_params)
+  	 flash[:success] = "配送先を変更しました"
+     redirect_to addresses_path
+	  else
+	   render "edit"
+	  end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+	  @address.destroy
+    @addresses = current_customer.address
+    flash.now[:alert] = "配送先を削除しました"
+	  # redirect_to addresses_path
   end
 
   def params_adresses
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_071410) do
+ActiveRecord::Schema.define(version: 2020_11_21_171119) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"

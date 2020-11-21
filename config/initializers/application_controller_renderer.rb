bc0054e11388495539b896baddcfc1dcac822380
EC2 Default User <wada.rio1001@gmail.com>
2020-11-21 05:20:52 +0000

    Fix design

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd16fc0ed7e046e5549a04b04fe74536970f9ad925b0b6e27d183e0b8dd8d415c7acb5963fe048a30a3c062f146555be2fe408a5f06db0a9fc21c60d9a76e09ff'

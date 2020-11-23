5682d0ffaa66a6d2231371b68a1db0437fe1ffe9
EC2 Default User <ec2-user@ip-172-31-66-186.ec2.internal>
2020-11-23 04:57:47 +0000

    [Add]バリデーション完了

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
+  validates :postal_code, :address, :name, presence: true
+  validates :postal_code,    length: { in: 7..7 }
+  
 end

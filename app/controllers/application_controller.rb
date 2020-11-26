3435d82457c91b0b4118d3fa7c1043a98063b414
EC2 Default User <kmsk4nmk3@gmail.com>
2020-11-26 03:28:13 +0000

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,8 +41,13 @@ class ApplicationController < ActionController::Base
     end
   end
   
+  # 会員用
   def after_sign_out_path_for(resource)
     new_customer_session_path
   end
-
+  # 管理者用
+  def after_sign_out_path_for(resource)
+      new_admin_session_path
+  end
+  
 end
